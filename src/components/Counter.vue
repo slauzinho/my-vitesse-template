369ab933dacdb873dd65bcdaffc9d252116a23f4
teerryn <slauzinho@users.noreply.github.com>
2022-11-08 14:51:14 +0000

    first commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const props = defineProps<{
+  initial: number
+}>()
+defineEmits<{
+  (event: 'test', count: string): void
+}>()
+
+const { count, inc, dec } = useCounter(props.initial)
+</script>
+
+<template>
+  <div>
+    {{ count }}
+    <button class="inc" @click="inc()">
+      +
+    </button>
+    <button class="dec" @click="dec()">
+      -
+    </button>
+  </div>
+</template>
